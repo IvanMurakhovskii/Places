1402bfd384369a2fc76fe99a5ea1d53b4dd24914
Ivan Murahovskiy <ivan112247@gmail.ru>
2018-07-03 19:36:55 +0300

    mapActivity

--- app/src/main/java/com/murik/places/MainActivity.java
+++ app/src/main/java/com/murik/places/MainActivity.java
@@ -1,11 +1,12 @@
 package com.murik.places;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Places;
 
 public class MainActivity extends AppCompatActivity
         implements GoogleApiClient.OnConnectionFailedListener{
@@ -16,6 +17,13 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+
+       //Places.PlaceDetectionApi.getCurrentPlace(mGoogleApiClient, null);
     }
 
     @Override
